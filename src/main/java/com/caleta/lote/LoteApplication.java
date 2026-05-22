294c95aaf6ee06f351880a9e59539a503685099b
BenjitaDEV <benjamin.cespedes2708@gmail.com>
2026-05-22 17:58:08 -0400

    lote

---
+++ src/main/java/com/caleta/lote/LoteApplication.java
@@ -0,0 +1,13 @@
+package com.caleta.lote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caleta/lote/LoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caleta.lote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
